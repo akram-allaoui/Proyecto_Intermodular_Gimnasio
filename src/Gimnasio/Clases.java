31992179fd81f150987d0a1fc2186f22dbdfb3b1
akram-allaoui <148786848+akram-allaoui@users.noreply.github.com>
2025-04-07 18:59:58 +0100

    Update Clases.java

--- src/Gimnasio/Clases.java
+++ src/Gimnasio/Clases.java
@@ -1,12 +1,27 @@
+/**
+ * @author Akram Allaoui
+ */
 package Gimnasio;
+/**
+ * Representa una clase grupal ofrecida en el gimnasio.
+ */
 
 public class Clases extends Sala {
-
-
+/**
+     * Constructor para crear una clase programada.
+     * @param nifCliente NIF del cliente que reserva
+     * @param codGimnasio Código del gimnasio 
+     * @param codigo Código único de la clase 
+     * @param clases Tipo de clase (1=Spinning, 2=Yoga,...)
+     * @param horario Horario en formato "Día HH:MM-HH:MM"
+     */
     public Clases(String nifCliente, int codGimnasio, int codigo, int clases, String horario) {
         super(nifCliente, codGimnasio, codigo, clases, horario);
     }
-
+/**
+     * Serializa la clase a formato JSON.
+     * @return Representación JSON con el código de clase
+     */
     public String toJson() {
         StringBuilder jsonBuilder = new StringBuilder();
 
@@ -19,7 +34,10 @@ public class Clases extends Sala {
         return jsonBuilder.toString();
 
     }
-
+/**
+     * Serializa la clase a formato XML.
+     * @return Representación XML con el código de clase
+     */
     public String toXml() {
         StringBuilder jsonBuilder = new StringBuilder();
 
