81f452619c858295ddf21fe2a5cac1781b91ed49
cristian11sm <criistan11sm@gmail.com>
2025-04-07 19:31:12 +0200

    Update Membresia.java

--- src/Gimnasio/Membresia.java
+++ src/Gimnasio/Membresia.java
@@ -1,36 +1,55 @@
+/**
+ * @author Cristian Sánchez Martínez
+ */
 package Gimnasio;
 
+/**
+ * Clase que representa una membresía de gimnasio, con su tipo y precio.
+ */
 public class Membresia {
 
-    private String tipo;
-    private double precio;
+    private String tipo;     // Tipo de membresía (ej: básica, premium, etc.)
+    private double precio;   // Precio de la membresía
 
-
-    public String toXml() {
-        StringBuilder jsonBuilder = new StringBuilder();
-
-        jsonBuilder.append("<membresia>\n")
-                .append("\t <tipo>: ").append(tipo).append("</tipo>\n")
-                .append("\t <precio>: ").append(precio).append("</precio>\n")
-                .append("</membresia>");
-
-        return jsonBuilder.toString();
-
+    /**
+     * Constructor de la clase Membresia.
+     * @param tipo Tipo de la membresía.
+     * @param precio Precio de la membresía.
+     */
+    public Membresia(String tipo, double precio) {
+        this.tipo = tipo;
+        this.precio = precio;
     }
 
+    /**
+     * Convierte los datos de la membresía a formato XML.
+     * @return Representación XML de la membresía.
+     */
+    public String toXml() {
+        StringBuilder xmlBuilder = new StringBuilder();
+
+        xmlBuilder.append("<membresia>\n")
+                .append("\t<tipo>").append(tipo).append("</tipo>\n")
+                .append("\t<precio>").append(precio).append("</precio>\n")
+                .append("</membresia>");
+
+        return xmlBuilder.toString();
+    }
+
+    /**
+     * Convierte los datos de la membresía a formato JSON.
+     * @return Representación JSON de la membresía.
+     */
     public String toJson() {
         StringBuilder jsonBuilder = new StringBuilder();
 
         jsonBuilder.append("{\n")
                 .append("\t\"Membresia\": {\n")
-                .append("\t \"Tipo\": ").append("\"" + tipo + "\"").append(",\n")
-                .append("\t \"Precio\": ").append("\"" + precio + "\"").append(",\n")
+                .append("\t\t\"Tipo\": \"").append(tipo).append("\",\n")
+                .append("\t\t\"Precio\": ").append(precio).append("\n")
                 .append("\t}\n")
                 .append("}");
 
         return jsonBuilder.toString();
-
     }
-
-
 }
