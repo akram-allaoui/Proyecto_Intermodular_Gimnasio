bc77e376d01e464351dff95795e3437c581bb9a0
ZianFranxes <109181577+ZianFranxes@users.noreply.github.com>
2025-04-07 19:44:05 +0200

    Update Trabajador.java

--- src/Gimnasio/Trabajador.java
+++ src/Gimnasio/Trabajador.java
@@ -1,17 +1,28 @@
 package Gimnasio;
 
+/**
+* Clase Trabajador, representa un empleado del gimnasio.
+* Contiene información personal como nombre, apellidos, NIF, fecha de nacimiento y correo electrónico.
+* @version 01-2025
+* @author Juan Franco
+* @since 07-04-2025
+*/
 public class Trabajador {
 
-    protected String nif;
-    protected String nombre;
-    protected String apellidos;
-    protected String fnac;
-    protected String email;
+    protected String nif;        // NIF del trabajador
+    protected String nombre;     // Nombre del trabajador
+    protected String apellidos;  // Apellidos del trabajador
+    protected String fnac;       // Fecha de nacimiento
+    protected String email;      // Correo electrónico
 
-    public void dirigir(){
-
-    }
-
+    /**
+    * Constructor de la clase Trabajador con todos los atributos.
+    * @param nif NIF del trabajador
+    * @param nombre Nombre del trabajador
+    * @param apellidos Apellidos del trabajador
+    * @param fnac Fecha de nacimiento del trabajador
+    * @param email Correo electrónico del trabajador
+    */
     public Trabajador(String nif, String nombre, String apellidos, String fnac, String email) {
         this.nif = nif;
         this.nombre = nombre;
@@ -20,77 +31,94 @@ public class Trabajador {
         this.email = email;
     }
 
+    /** Método vacío que representa acción de dirigir */
+    public void dirigir() {}
 
+    /** @return NIF del trabajador */
     public String getNif() {
         return nif;
     }
 
+    /** @param nif Nuevo NIF del trabajador */
     public void setNif(String nif) {
         this.nif = nif;
     }
 
+    /** @return Nombre del trabajador */
     public String getNombre() {
         return nombre;
     }
 
+    /** @param nombre Nuevo nombre del trabajador */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /** @return Apellidos del trabajador */
     public String getApellidos() {
         return apellidos;
     }
 
+    /** @param apellidos Nuevos apellidos del trabajador */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    /** @return Fecha de nacimiento del trabajador */
     public String getFnac() {
         return fnac;
     }
 
+    /** @param fnac Nueva fecha de nacimiento */
     public void setFnac(String fnac) {
         this.fnac = fnac;
     }
 
+    /** @return Email del trabajador */
     public String getEmail() {
         return email;
     }
 
+    /** @param email Nuevo email del trabajador */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+    * Devuelve una representación XML del trabajador.
+    * @return Cadena en formato XML con los datos del trabajador
+    */
     public String toXml() {
-        StringBuilder jsonBuilder = new StringBuilder();
+        StringBuilder xmlBuilder = new StringBuilder();
 
-        jsonBuilder.append("<trabajador>\n")
-                .append("\t <nif>: ").append(nif).append("</nif>\n")
-                .append("\t <nombre>: ").append(nombre).append("</nombre>\n")
-                .append("\t <apellidos>: ").append(apellidos).append("</apellidos>\n")
-                .append("\t <fecha-de-nacimiento>: ").append(fnac).append("</fecha-de-nacimiento>\n")
-                .append("\t <email>: ").append(email).append("</email>\n")
+        xmlBuilder.append("<trabajador>\n")
+                .append("\t<nif>").append(nif).append("</nif>\n")
+                .append("\t<nombre>").append(nombre).append("</nombre>\n")
+                .append("\t<apellidos>").append(apellidos).append("</apellidos>\n")
+                .append("\t<fecha-de-nacimiento>").append(fnac).append("</fecha-de-nacimiento>\n")
+                .append("\t<email>").append(email).append("</email>\n")
                 .append("</trabajador>");
 
-        return jsonBuilder.toString();
-
+        return xmlBuilder.toString();
     }
 
+    /**
+    * Devuelve una representación JSON del trabajador.
+    * @return Cadena en formato JSON con los datos del trabajador
+    */
     public String toJson() {
         StringBuilder jsonBuilder = new StringBuilder();
 
         jsonBuilder.append("{\n")
                 .append("\t\"Trabajador\": {\n")
-                .append("\t \"Nif\": ").append("\"" + nif + "\"").append(",\n")
-                .append("\t \"Nombre\": ").append("\"" + nombre + "\"").append(",\n")
-                .append("\t \"Apellidos\": ").append("\"" + apellidos + "\"").append(",\n")
-                .append("\t \"Fecha de nacimiento\": ").append("\"" + fnac + "\"").append(",\n")
-                .append("\t \"Email\": ").append("\"" + email + "\"").append(",\n")
+                .append("\t\t\"Nif\": ").append("\"").append(nif).append("\",\n")
+                .append("\t\t\"Nombre\": ").append("\"").append(nombre).append("\",\n")
+                .append("\t\t\"Apellidos\": ").append("\"").append(apellidos).append("\",\n")
+                .append("\t\t\"Fecha de nacimiento\": ").append("\"").append(fnac).append("\",\n")
+                .append("\t\t\"Email\": ").append("\"").append(email).append("\"\n")
                 .append("\t}\n")
                 .append("}");
 
         return jsonBuilder.toString();
-
     }
-
 }
